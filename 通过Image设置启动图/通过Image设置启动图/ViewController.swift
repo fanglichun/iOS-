109c502b75a55078466c9f1528c18d16c3ecf346
mfang032 <michal.fang@pwc.com>
2016-09-15 19:50:55 +0800

    No msg

---
+++ 通过Image设置启动图/通过Image设置启动图/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  通过Image设置启动图
+//
+//  Created by mfang032 on 16/9/15.
+//  Copyright © 2016年 mfang032. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
